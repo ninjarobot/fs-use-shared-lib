f7caa33e86db894e0092be87200574b2f9f040af
Dave Curylo <curylod@asme.org>
2016-09-07 21:35:45 -0400

    Adding example for using a native shared library via P/invoke from FSharp.

---
+++ fsUseSharedLib/Program.fs
@@ -0,0 +1,18 @@
+﻿open System.Runtime.InteropServices
+open System.Text
+
+[<DllImport("libmylib")>]
+extern void hello()
+[<DllImport("libmylib", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)>]
+extern void createString(nativeint& s)
+
+[<EntryPoint>]
+let main argv =
+    hello()
+    let mutable pStr = Marshal.AllocHGlobal(256)
+    createString(&pStr)
+    let s = Marshal.PtrToStringAnsi(pStr)
+    Marshal.FreeHGlobal(pStr)
+    printfn "%s" s
+    0 // return an integer exit code
+
